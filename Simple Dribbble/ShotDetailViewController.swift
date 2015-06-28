6f0738c6be4873385e8d5e1aa4c37a76c4db7438
Clêrton Leal <clertonleal@gmail.com>
2015-06-28 12:52:47 -0300

    Inicio de detalhes

--- Simple Dribbble/ShotDetailViewController.swift
+++ Simple Dribbble/ShotDetailViewController.swift
@@ -14,14 +14,15 @@ import Kingfisher
 class ShotDetailViewController: UIViewController {
     
     @IBOutlet var text: UILabel!
-    
     @IBOutlet var image: UIImageView!
+    @IBOutlet var viewCount: UILabel!
     
     var shot: Shot!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         text.text = shot.title!
+        viewCount.text = String(shot.views_count!)
         downloadImage(shot.image_url!)
     }
     
